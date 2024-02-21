98fbc095d75a50165eb67f420464ca8ba090415d
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2024-02-21 17:13:19 +0100

    ADDED : DTO register

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 using System.Security.Cryptography;
 using System.Text;
+using API.DTOs;
 using API.Models;
 using API.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -15,25 +17,33 @@ namespace API.Controllers
             _context = context;
         }
         [HttpPost("register")]
-        public async Task<ActionResult<User>> Register(string email, string password)
+        public async Task<ActionResult<User>> Register( RegisterDto registerDto)
         {
+               if (await UserExists(registerDto.Email))
+        return BadRequest("Email is taken");
             using var hmac = new HMACSHA512();
             var user = new User
             {
-                FirstName = email,
-                LastName = email,
-                Email = email,
-                PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                FirstName = registerDto.Email.ToLower(),
+                LastName = registerDto.Email.ToLower(),
+                Email = registerDto.Email,
+                PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key,
                 Role = Models.User.UserRole.User,
                 Gender = Models.User.UserGender.NA,
                 IsDeleted = false,
                 Picture = "https://res.cloudinary.com/dk5b3jxjp/image/upload/v1633660733/astree/placeholder.png",
-                nickName=email,
+                nickName=registerDto.Email,
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return user;
         }
+                private async Task<bool> UserExists(string email)
+        {
+            return await _context.Users.AnyAsync(user => user.Email == email.ToLower());
+        }
+
+
     }
 }
