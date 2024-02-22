4c0ee76ebb8db7e5e30b70979e9f9a21714edcf4
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2024-02-22 16:46:15 +0100

    update login and register in the client app

--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -4,8 +4,8 @@ namespace API.DTOs
 {
     public class LoginDto
     {
-                [Required]
-            [EmailAddress]
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
